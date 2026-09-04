e41355a6e0e173f904eb926ec240fe140096f408
KennethH <saffrongamer167@gmail.com>
2026-09-04 09:18:13 -0600

    Added error handling and null checking

--- src/FishDex/ViewModels/RecordCatchViewModel.cs
+++ src/FishDex/ViewModels/RecordCatchViewModel.cs
@@ -25,10 +25,20 @@ namespace FishDex.ViewModels
         });
         public Command PickPhotosAsyncCommand => field ??= new Command(async () =>
         {
+            var nav = Application.Current?.MainPage;
             List<FileResult>? photo = await MediaPicker.Default.PickPhotosAsync();
-            if (photo != null)
-                await photo?.FirstOrDefault()?.OpenReadAsync();
-            
+            if (photo != null && nav != null)
+                try
+                {
+                    await photo?.FirstOrDefault()?.OpenReadAsync();
+                } catch (Exception ex)
+                {
+                    await nav.DisplayAlertAsync("Could not open photo", ex.Message, "OK");
+                }
+            else if (nav != null)
+            {
+                await nav.DisplayAlertAsync("No photo selected", "You did not select a photo.", "OK");
+            }
         });
         public Command TakePhotoAsyncCommand => field ??= new Command(async () =>
         {
