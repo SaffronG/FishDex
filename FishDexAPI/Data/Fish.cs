ee95477bf5ffeab6a6e064c34e06e5a55b6a0a49
bradley.mccormick <bradley.mccormick@students.snow.edu>
2026-09-04 13:29:24 -0600

    commit befor migration

---
+++ FishDexAPI/Data/Fish.cs
@@ -0,0 +1,13 @@
+﻿namespace FishDex.API.Data
+{
+    public class Fish(int id, string name, int weight, int length)
+    {
+        public int Id { get; set; } = id;
+
+        public string Name { get; set; } = name;
+        public int Weight { get; set; } = weight;
+
+        public int Length { get; set; }
+
+    }
+}
