20d335e28ae04af62538216bd69a5bb4b4a86a97
EthanHintze <ethanhintze011@gmail.com>
2026-09-10 17:11:10 -0600

    Logs show in console, not structured

--- FishDexAPI/Program.cs
+++ FishDexAPI/Program.cs
@@ -1,5 +1,7 @@
 using FishDex.API.Data;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<List<string>>(_ => FishData.Seed());
@@ -23,8 +25,17 @@ builder.Services.AddOutputCache(options =>
 builder.Services.AddOpenApi("dev"); // Open Api route is {ROOT}/openapi/dev.json
 
 builder.Services.AddDbContext<FishDbContext>(o => o.UseNpgsql());
-
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+    logging.AddOtlpExporter(otlpOptions =>
+    {
+        otlpOptions.ExportProcessorType = ExportProcessorType.Simple; //Causes more overhead because more calls are made
+    });
+});
 var app = builder.Build();
+
 //Logging and lifetime services
 var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
