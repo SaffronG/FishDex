5788445d6b69c48a0189cebd253487cfcbd3aa90
bradley.mccormick <bradley.mccormick@students.snow.edu>
2026-09-11 11:55:14 -0600

    profile branch created

---
+++ src/FishDex/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FishDex.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FishDex.ViewModels
+{
+    public partial class ProfilePageViewModel
+    {
+        
+        public string name {  get; set; }
+
+
+        public ProfilePageViewModel( string _name)
+        {
+           name = _name;
+        }
+
+        private readonly IApiService _apiService;
+        private readonly INavigationService _navigationService;
+    }
+}
